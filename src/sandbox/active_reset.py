7b9798c8739e9d342ad5b58291e3916579a72bf5
Aaron Vontell <vontell@mit.edu>
2017-10-03 18:32:27 -0400

    Fixed initial experiment

--- src/sandbox/active_reset.py
+++ src/sandbox/active_reset.py
@@ -2,18 +2,23 @@ from artiq.experiment import *
 from pipistrello import Board
 
 class ActiveReset(EnvExperiment):
-    '''
-    Does an active reset of a given qubit.
-    '''
-
-    def build(self):
-        self.board = Board(self)
-
-    @kernel
-    def run(self):
-        
-        START = now_mu()
+	'''
+	Does an active reset of a given qubit.
+	'''
+	
+	def build(self):
+		self.board = Board(self)
+		
+	@kernel
+	def run(self):
+		
+		self.board.reset()
+		self.board.get_core().break_realtime()
+		delay(2*s)
+		START = now_mu()
 		print("Start time: ", START)
 		print("Starting pulse")
-		self.board.pulse(0, 0.6 * ns, 1)
+		self.board.pulse(0, .6 * ns, 1)
+		delay(10 * ns)
+		self.board.pulse(0, .6 * ns, 1)
 		print("Pulses placed. Done!")
